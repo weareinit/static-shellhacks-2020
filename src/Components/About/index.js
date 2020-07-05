97ce839108d6bd66a359ebb4d56c947c0cd27948
Yasmine <yasminea0217@gmail.com>
2020-07-05 17:54:15 -0400

    updated about section text

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -15,36 +15,10 @@ const AboutSection = props => {
       <div className="AboutSection__content">
         <h2 className="AboutSection__title">Enter A New Dimension!</h2>
         <p className="AboutSection__info">
-            Ready for a weekend full of learning, innovation and fun? Come join us at ShellHacks, Florida's Largest Hackathon! Explore our tech city through our various workshops, fun activities, networking opportunites and more. Create innovative projects and meet students from across the nations as we showcase the various technologies used in the industry!
+        Ready for a weekend full of learning, innovation, and fun? Come and join us at ShellHacks, Florida's Largest Hackathon! Enter a new dimension as 1,000+ students from around the world come to learn the latest technologies, build innovative projects, and showcase their talents. 
+        <br/><br/>Explore the world of technology through our learning tracks and interactive workshops. Work together with fellow students and mentors to develop projects solving exciting challenges. Meet with recruiters from top companies hiring students for internships and job opportunities. And win amazing prizes - all at ShellHacks!
         </p>
-        <div className="AboutSection__details">
-          <ul className="AboutSection__list">
-            <li className="AboutSection__detail">
-              <i className="AboutSection__icon">
-                <img className="AboutSection__iconImg" src={learnEmoji} alt="Learn"/>
-              </i>
-              <p className="AboutSection__detailInfo">
-                <span>Learn</span> from 20+ workshops covering topics such as cybersecurity, hardware, game development and more!
-              </p>
-            </li>
-            <li className="AboutSection__detail">
-              <i className="AboutSection__icon">
-                <img className="AboutSection__iconImg" src={relaxEmoji} alt="Learn"/>
-              </i>
-                <p className="AboutSection__detailInfo">
-                  <span>Relax</span> and sit back with other participants as you join us in one of our various activities.
-                </p>
-            </li>
-            {/* <li className="AboutSection__detail">
-              <i className="AboutSection__icon">
-                <img className="AboutSection__iconImg" src={treatEmoji} alt="Learn"/>
-              </i>
-              <p className="AboutSection__detailInfo">
-                <span>Treat</span> yourself to some of our "Miami" cuisine and more
-              </p>
-            </li> */}
-          </ul>
-        </div>
+        
       </div>
     </div>
   )
