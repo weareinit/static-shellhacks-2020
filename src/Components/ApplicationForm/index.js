bf73af85a47d607a0e871fb702d64d8a769afcde
Kevin Losada <kevin.losada21@gmail.com>
2020-08-13 13:13:07 -0400

    Commented out in progress checkbox

--- src/Components/ApplicationForm/index.js
+++ src/Components/ApplicationForm/index.js
@@ -363,12 +363,12 @@ const SignUpForm = props => {
             }
           ]
         },
-        {
-          name: 'checkbox',
-          label: 'You give us permission to share your email with sponsors',
-          placeholder: '',
-          type: 'boolean'
-        },
+        // {
+        //   name: 'checkbox',
+        //   label: 'You give us permission to share your email with sponsors',
+        //   placeholder: '',
+        //   type: 'boolean'
+        // },
         {
           name: 'resume',
           label: 'Resume * (PDF only)',
